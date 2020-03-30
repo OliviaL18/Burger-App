7cb94b48d913cac0412f6d7e464c2b1e5e451a1b
OliviaL18 <oliviatloos@gmail.com>
2020-03-29 21:17:06 -0400

    fixed add burger

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,4 +1,3 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
   $(".change-eaten").on("click", function(event) {
     const id = $(this).data("id");
@@ -8,35 +7,30 @@ $(function() {
       eaten: neweaten
     };
 
-    // Send the PUT request.
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: neweatenState
     }).then(
       function() {
         console.log("changed eaten to", neweaten);
-        // Reload the page to get the updated list
         location.reload();
       }
     );
   });
 
   $(".create-form").on("submit", function(event) {
-    // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
     const newBurger = {
-      name: $("#burger").val().trim(),
+      burgerName: $("#burger").val().trim(),
     };
 
-    // Send the POST request.
     $.ajax("/api/burgers", {
       type: "POST",
       data: newBurger
     }).then(
       function() {
         console.log("created new burger");
-        // Reload the page to get the updated list
         location.reload();
       }
     );
@@ -45,13 +39,11 @@ $(function() {
   $(".delete-burger").on("click", function(event) {
     const id = $(this).data("id");
 
-    // Send the DELETE request.
     $.ajax("/api/burgers/" + id, {
       type: "DELETE"
     }).then(
       function() {
         console.log("deleted burger", id);
-        // Reload the page to get the updated list
         location.reload();
       }
     );
